865b7737a71b049b196f996030768b79791a4b03
Genesis Enedeh <genesis.enedeh@gmail.com>
2021-05-16 20:43:07 +0100

    added some more html and functionality of the view

--- document_writer/document/views.py
+++ document_writer/document/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
+from .models import Document
 
 
 # Create your views here.
 def home_view(request):
-    return render(request, "home.html", {'hello': 'hello from'})
+    return render(request, "home.html", {'document': Document.objects.all})
