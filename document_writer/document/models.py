5f388b4575a42877c10288bcb1b4416c0e323210
Genesis Enedeh <genesis.enedeh@gmail.com>
2021-05-16 19:19:26 +0100

    Changed Document to Documents and migrated

--- document_writer/document/models.py
+++ document_writer/document/models.py
@@ -5,9 +5,9 @@ from random import randint
 # Create your models here.
 class Document(models.Model):
     doc_name = models.CharField(verbose_name="document_name", default=f"Document{randint(1029283, 918829102832123)}",
-                                max_length=120, blank=True)
+                                max_length=120, blank=True, unique=True)
     doc = models.TextField(verbose_name="document")
     about = models.TextField(help_text="Write some description about the document", default="", blank=True)
-    doc_img = models.ImageField(default="doc_image.png", verbose_name="document_img")
+    doc_img = models.ImageField(default="/static/doc_image.png", verbose_name="document_img")
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
