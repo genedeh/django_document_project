8780ffb51b656f212ea84e2854acbb53cc55c45d
Genesis Enedeh <genesis.enedeh@gmail.com>
2021-05-16 21:28:32 +0100

    adding a view and url to make a detail page

--- document_writer/document/urls.py
+++ document_writer/document/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import home_view
+from .views import home_view, DocumentDetailView
 
 app_name = "document"
 
 urlpatterns = [
-    path('', home_view, name="home")
+    path('', home_view, name="home"),
+    path('detail/<pk>/', DocumentDetailView.as_view(), name="detail")
 ]
--- document_writer/document/views.py
+++ document_writer/document/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from .models import Document
-
+from django.views.generic import DetailView
 
 # Create your views here.
 def home_view(request):
     return render(request, "home.html", {'document': Document.objects.all})
+
+
+class DocumentDetailView(DetailView):
+    model = Document
+    template_name = 'document/detail.html'
