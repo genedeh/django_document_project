a52ad3ecbb0b9247a7c68950c6faf51c44c2320e
Genesis Enedeh <genesis.enedeh@gmail.com>
2021-05-16 19:04:15 +0100

    had an error changed charfield to Textfield

--- document_writer/document/models.py
+++ document_writer/document/models.py
@@ -6,8 +6,8 @@ from random import randint
 class Documents(models.Model):
     doc_name = models.CharField(verbose_name="document_name", default=f"Document{randint(1029283, 918829102832123)}",
                                 max_length=120, blank=True)
-    doc = models.CharField(verbose_name="document")
-    about = models.CharField(help_text="Write some description about the document", default="", blank=True)
+    doc = models.TextField(verbose_name="document")
+    about = models.TextField(help_text="Write some description about the document", default="", blank=True)
     doc_img = models.ImageField(default="doc_image.png", verbose_name="document_img")
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
