d0cd1ea6758cab6041899d3786bcbfa77dde01cf
Genesis Enedeh <genesis.enedeh@gmail.com>
2021-05-16 18:59:59 +0100

    created a model called Documents

--- document_writer/document/models.py
+++ document_writer/document/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from random import randint
+
 
 # Create your models here.
+class Documents(models.Model):
+    doc_name = models.CharField(verbose_name="document_name", default=f"Document{randint(1029283, 918829102832123)}",
+                                max_length=120, blank=True)
+    doc = models.CharField(verbose_name="document")
+    about = models.CharField(help_text="Write some description about the document", default="", blank=True)
+    doc_img = models.ImageField(default="doc_image.png", verbose_name="document_img")
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
