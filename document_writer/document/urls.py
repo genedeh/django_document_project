7657f0ba5d961db4e8f38d516a6df3e271df8a72
Genesis Enedeh <genesis.enedeh@gmail.com>
2021-05-16 22:59:39 +0100

    Added some changes into detail.html

--- document_writer/document/urls.py
+++ document_writer/document/urls.py
@@ -4,6 +4,6 @@ from .views import home_view, DocumentDetailView
 app_name = "document"
 
 urlpatterns = [
-    path('', home_view, name="home"),
+    path('', home_view, name='home'),
     path('detail/<pk>/', DocumentDetailView.as_view(), name="detail"),
 ]
