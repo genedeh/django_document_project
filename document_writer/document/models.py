755c5c4dbaff6edaec60c25f4243e5778b77475f
Genesis Enedeh <genesis.enedeh@gmail.com>
2021-05-18 22:30:41 +0100

    added a help text to document and migrated

--- document_writer/document/models.py
+++ document_writer/document/models.py
@@ -10,7 +10,7 @@ from fpdf import FPDF
 class Document(models.Model):
     doc_name = models.CharField(verbose_name="document_name", default=f"Document{randint(1029283, 918829102832123)}",
                                 max_length=250, blank=True, unique=True)
-    doc = models.TextField(verbose_name="document")
+    doc = models.TextField(verbose_name="document", help_text="you cannot add an emoji")
     about = models.TextField(help_text="Write some description about the document", default="", blank=True)
     doc_img = models.ImageField(default="doc_image.png", verbose_name="document_img")
     created = models.DateTimeField(auto_now_add=True)
